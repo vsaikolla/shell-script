5609345b5d1942f079fc05a7b58f5cd00ac5194f
sainath <kollavenkatasai999@gmail.com>
2025-08-05 11:51:22 -0400

    first commit

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#variable starts with $
+#there shouldn't be any space between any variable and its value
+
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1 :: Hi $PERSON2, How are you doing?"
+echo "$PERSON2 :: Hi $PERSON1, I am good and how about you?"
