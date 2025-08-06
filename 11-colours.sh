0ca7b5c03ded8b839fb6a999680a9a69e5edc66e
sainath <kollavenkatasai999@gmail.com>
2025-08-06 13:24:35 -0400

    functions

---
+++ 11-colours.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ..... $R Failure $N"
+        exit 2
+    else
+        echo -e "$2...... $G Success $N"
+    fi 
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo " Please run this script with root access "
+    exit 1 # manual exit if error occurs ( 1 to 127 for exit status)
+else
+    echo " You're super user "
+fi
+
+
+dnf install mysql -y &>> $LOGFILE
+VALIDATE $? "Installing MYSQL"
+
+dnf install git -y &>> $LOGFILE
+VALIDATE $? "Installing Git"
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of all packages is Failure"
+else
+    echo "Installation of all packages is Success"
+fi
---
+++ 12-install-packages.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ..... $R Failure $N"
+        exit 2
+    else
+        echo -e "$2...... $G Success $N"
+    fi 
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo " Please run this script with root access "
+    exit 1 # manual exit if error occurs ( 1 to 127 for exit status)
+else
+    echo " You're super user "
+fi
+
+for i in $@
+do
+    echo "Package to install : $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i is already installed... $Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
