5627c86ec16a82017081de0d2e04cf255fce5a68
sainath <kollavenkatasai999@gmail.com>
2025-08-06 10:04:52 -0400

    special vars

---
+++ 07-special-vars.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "All variables: $@"
+
+echo "Number of variables passed: $#"
+
+echo "Script name: $0"
+
+echo "current working directory: $PWD"
+
+echo "home directory of a user: $HOME"
+
+echo "user running the script: $USER"
+
+echo "hostname : $HOSTNAME"
+
+echo "process ID of the current shell script : $$"
+
+echo ""
